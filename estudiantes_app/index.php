94892493f87b13a97111444301ae1a27b085a622
Luaniki <jorgeluis04avellacorrea@gmail.com>
2022-05-07 17:50:25 -0500

    Transcripción del código

---
+++ estudiantes_app/index.php
@@ -0,0 +1,62 @@
+<?php
+require_once dirname(__DIR__) . '/estudiantes_app/db/conexion_db.php';
+require_once dirname(__DIR__) . '/estudiantes_app/controllers/i_controller.php';
+
+require_once dirname(__DIR__) . '/estudiantes_app/models/estudiante.php';
+require_once dirname(__DIR__) . '/estudiantes_app/controllers/estudiante_controller.php';
+
+use controllers\EstudianteController;
+
+$estudianteController = new EstudianteController();
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Estudiantes</title>
+</head>
+
+<body>
+    <h1>Lista de estudiantes</h1>
+    <a href="form_estudiantes.php">Registrar nuevo estudiante</a>
+    <table>
+        <thead>
+            <tr>
+                <th>Código</th>
+                <th>Estudiante</th>
+                <th>Edad</th>
+                <th>Modificar</th>
+                <th>Eliminar</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $estudientes = $estudianteController->list();
+            if (count($estudientes) > 0) {
+                foreach ($estudientes as $estudiente) {
+                    echo '<tr>';
+                    echo ' <td>' . $estudiente->get('codigo') . '</td>';
+                    echo ' <td>' . $estudiente->get('nombres') . ' ' . $estudiente->get('apellidos') . '</td>';
+                    echo ' <td>' . $estudiente->get('edad') . '</td>';
+                    echo ' <td>';
+                    echo '   <a href="form_estudiantes.php?idE=' . $estudiente->get('id') . '">Modificar</a>';
+                    echo ' </td>';
+                    echo ' <td>';
+                    echo '   <a href="eliminar.php?idE=' . $estudiente->get('id') . '">Eliminar</a>';
+                    echo ' </td>';
+                    echo '</tr>';
+                }
+            } else {
+                echo '<tr>';
+                echo ' <td colspan="3">No hay registros</td>';
+                echo '</tr>';
+            }
+            ?>
+        </tbody>
+    </table>
+</body>
+
+</html>
---
+++ estudiantes_app/registrar.php
@@ -0,0 +1,45 @@
+<?php
+require_once dirname(__DIR__) . '/estudiantes_app/db/conexion_db.php';
+require_once dirname(__DIR__) . '/estudiantes_app/controllers/i_controller.php';
+
+require_once dirname(__DIR__) . '/estudiantes_app/models/estudiante.php';
+require_once dirname(__DIR__) . '/estudiantes_app/controllers/estudiante_controller.php';
+
+use controllers\EstudianteController;
+use models\Estudiante;
+
+$estudianteController = new EstudianteController();
+
+$estudiante = new Estudiante();
+$estudiante->set('codigo', $_POST['codigoInput']);
+$estudiante->set('nombres', $_POST['nombresInput']);
+$estudiante->set('apellidos', $_POST['apellidosInput']);
+$estudiante->set('edad', $_POST['edadInput']);
+
+$resultado = $estudianteController->create($estudiante);
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Registro</title>
+</head>
+
+<body>
+    <h1>Resultados de la operación</h1>
+    <?php
+    if ($resultado) {
+        echo '<p>Registro exitoso</p>';
+        echo '<br>';
+        echo '<a href="index.php">Volver a la lista</a>';
+    } else {
+        echo '<p>Se presentó un error al guardar los datos. Vuelva a intentar.</p>';
+        echo '<br>';
+        echo '<a href="form_estudiantes.php">Volver</a>';
+    }
+    ?>
+</body>
+
+</html>
